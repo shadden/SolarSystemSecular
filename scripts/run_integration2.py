74dcf6a93ef5c509d52db7aa049f8462299875be
Sam Hadden <hadden@rein004.utsc.utoronto.ca>
2023-01-20 11:59:40 -0500

    changed path to output files

--- scripts/run_integration2.py
+++ scripts/run_integration2.py
@@ -13,7 +13,7 @@ ig = initialize_integrator(5000)
 y0 = generate_inits(amd_tot,amd_tot)
 #finame="./g1_0p01_s1_0p01_realization{}".format(file_id)
 
-finame="./scaled_amd_sims/scaled_amd_val_{}_realization_{}".format(amd_frac_i,file_id)
+finame="../output/scaled_amd_sims/scaled_amd_val_{}_realization_{}".format(amd_frac_i,file_id)
 Tfin =  5e9
 Nout =  2**14
 run_integration(ig,y0,Tfin,Nout,outfile=finame)
